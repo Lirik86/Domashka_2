82f95c7cd9f9cc8b0b6efdf48591c90c30e45481
lirik <lightlirikus@yandex.ru>
2023-02-22 22:17:30 +0300

    выполнил первое задание

---
+++ zadanie1/Program.cs
@@ -0,0 +1,22 @@
+﻿int promt(string msg)
+{
+    System.Console.Write($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+bool validate(int num)
+{
+    bool r = num >= 100 && num < 1000;
+    if (r)
+    {
+        return true;
+    }
+    Console.WriteLine("ввели не верное число ");
+    return false;
+}
+
+int first = promt("Введите трехзначное число >");
+if(validate(first))
+{
+   int sec = (first / 10) % 10;
+    Console.WriteLine($"Вторая цифра в числе {first} равна {sec}");
+}
